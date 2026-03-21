cff13a9865fb456f9889af2732b414e8852af362
0xlakhe <249052680+0xlakhe@users.noreply.github.com>
2026-03-21 10:44:29 +0545

    Register/login/logout

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -3,21 +3,6 @@ from django.http import HttpResponse
 from .models import Post
 # Create your views here.
 
-posts = [
-    {
-        "author": "Hello",
-        "title": "Post 1",
-        "content": "First post content",
-        "date_posted": "August 41, 1998",
-    },
-    {
-        "author": "Hello2",
-        "title": "Post 2",
-        "content": "Second post content",
-        "date_posted": "November 03, 2000",
-    },
-]
-
 
 def home(request):
     context = {"posts": Post.objects.all()}
--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -122,3 +122,7 @@ STATIC_URL = "static/"
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
 CRISPY_TEMPLATE_PACK = "bootstrap4"
+
+LOGIN_REDIRECT_URL = "blog-home"
+LOGOUT_REDIRECT_URL = "login"
+LOGIN_URL = "login" 
