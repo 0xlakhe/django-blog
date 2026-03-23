a6a07201a004afc185380703abaafd58f557c1f5
0xlakhe <249052680+0xlakhe@users.noreply.github.com>
2026-03-23 12:33:45 +0545

    feat(blog): add class-based post list/detail views and link home posts to detail page

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from django.http import HttpResponse
 from . import views
+from .views import PostListView, PostDetailView
 
 urlpatterns = [
-    path("", views.home, name="blog-home"),
+    path("", PostListView.as_view(), name="blog-home"),
+    path("post/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
     path("about/", views.about, name="blog-about"),
     path("hello/", views.hello, name="hello"),
 ]
